a58917046c79050fc4cec1fa0d8996223970eb24
Dawid <dawidbasa@gmail.com>
2017-12-04 22:42:10 +0100

    added new script - insert into credits

--- src/test/resources/insert-test-data.sql
+++ src/test/resources/insert-test-data.sql
@@ -13,11 +13,15 @@ CREATE TABLE role (role_id INTEGER NOT NULL auto_increment, role VARCHAR(255), P
 CREATE TABLE user (user_id INTEGER NOT NULL auto_increment, active INTEGER, email VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY (user_id))
 CREATE TABLE user_role (user_id INTEGER NOT NULL, role_id INTEGER NOT NULL, PRIMARY KEY (user_id, role_id))
 ###########################################################################################################################################################################################################################################################################################
--- POPULATE DATABASE
+-- POPULATE USERS
 
 INSERT INTO role VALUES (1,'ADMIN')
 ###########################################################################################################################################################################################################################################################################################
-INSERT INTO user (active, email, last_name, name, password) VALUES (1, 'test@test.pl', 'test', 'test', '$2a$10$xnFGF7XYfd0J.F9J/5qFfOwqSPLC6B4YLcmZVceOF44GESTnR7VY6')
+INSERT INTO user (active, email, last_name, name, password) VALUES (1, 'test@test.pl', 'test', 'test', '$2a$10$txVjCtz1rryTAV3WlSUvtuF2JxBnv4egAEgrTNfEpRul2l5.hbM8a')
 ###########################################################################################################################################################################################################################################################################################
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1)
 ###########################################################################################################################################################################################################################################################################################
+-- POPULATE CREDITS
+
+INSERT INTO credit (commision_fee, credit_margin, mortgage_debt, name, mortgage_term, wibor) VALUES (5000, 3, 300000, 'PKO', 30, 2)
+INSERT INTO credit (commision_fee, credit_margin, mortgage_debt, name, mortgage_term, wibor) VALUES (4000, 2, 200000, 'NBP', 30, 2)
