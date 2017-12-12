244eee8ccbc2f79296a3f33f16151396bc9ce6a5
Dawid <dawidbasa@gmail.com>
2017-12-12 21:04:30 +0100

    login controller test - passed

--- src/test/java/pl/dawidbasa/crediAnalyser/LoginTest/LoginControllerTest.java
+++ src/test/java/pl/dawidbasa/crediAnalyser/LoginTest/LoginControllerTest.java
@@ -1,5 +1,36 @@
 package pl.dawidbasa.crediAnalyser.LoginTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import pl.dawidbasa.crediAnalyser.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {Application.class})
+@AutoConfigureMockMvc
 public class LoginControllerTest {
 
+	@Autowired
+	WebApplicationContext context;
+	@Autowired
+    private MockMvc mockMvc;
+	
+	@Test 
+	public void testShowLoginPage() throws Throwable  {
+		mockMvc.perform(get("/login"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("login"));
+	}
+
+	
 }
