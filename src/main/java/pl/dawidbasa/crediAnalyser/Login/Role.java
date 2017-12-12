b862ae0947d13633d5bc489d0e934cce98d62574
Dawid <dawidbasa@gmail.com>
2017-12-12 21:03:30 +0100

    deleted unused constructor

--- src/main/java/pl/dawidbasa/crediAnalyser/Login/Role.java
+++ src/main/java/pl/dawidbasa/crediAnalyser/Login/Role.java
@@ -7,15 +7,27 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+	@Entity
 	@Table(name = "role")
 	public class Role {
+		
 		@Id
 	    @GeneratedValue(strategy = GenerationType.AUTO)
 		@Column(name="role_id")
 		private int id;
+		
 		@Column(name="role")
 		private String role;
+	
+		public Role() {
+			super();
+		}
+
+		public Role(int id, String role) {
+			super();
+			this.id = id;
+			this.role = role;
+		}
 		
 		public int getId() {
 			return id;
