edc76a6146639f4bb444a50f1b7f217e07dcf07a
Dawid <dawidbasa@gmail.com>
2017-12-15 21:46:36 +0100

    Atm no confiugration is needed

--- src/main/java/pl/dawidbasa/crediAnalyser/configuration/MvcConfiguration.java
+++ src/main/java/pl/dawidbasa/crediAnalyser/configuration/MvcConfiguration.java
@@ -1,16 +1,10 @@
 package pl.dawidbasa.crediAnalyser.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfiguration extends WebMvcConfigurerAdapter {
+	
+	}
 
-/*	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		return bCryptPasswordEncoder;
-	}*/
-}
